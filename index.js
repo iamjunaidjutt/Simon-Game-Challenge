ca7de1209799113913cf1a3a14eb5589297c215a
iamjunaidjutt <engr.muhammadjunaid@hotmail.com>
2022-07-15 00:04:50 +0500

    Add click event function and playSound function

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 
 let gamePattern = [];
 let buttonColors = ["red", "blue", "green", "yellow"];
-
+let userClickedPattern = [];
 
 function nextSequence() {
     let randomNum = Math.floor(Math.random() * 4);
-    return randomNum;
+    let randomChosenColor = buttonColors[nextSequence()]; 
+    gamePattern.push(randomChosenColor);
+    
+    $("#"+randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    let audio = new Audio("./sounds/"+randomChosenColor+".mp3");
+    audio.play();   
+}
+$(".btn").on("click", function() {
+    let userChosenColor =  $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+})
+
+function playSound(name) {
+    let audio = new Audio("./sounds/"+name+".mp3");
+    audio.play(); 
 }
 
-let randomChosenColor = buttonColors[nextSequence()]; 
-gamePattern.push(randomChosenColor);
-
-$("#"+randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
