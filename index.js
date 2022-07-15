1823a9a9c2d1a8fb21d27708dc0fc4f63b98962f
iamjunaidjutt <engr.muhammadjunaid@hotmail.com>
2022-07-15 11:31:17 +0500

    Add checkAnswer function when button is clicked

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ let gamePattern = [];
 let buttonColors = ["red", "blue", "green", "yellow"];
 let userClickedPattern = [];
 let level = 0;
-let started = false;
+let started = false;    
 
 function nextSequence() {
+    userClickedPattern = [];
     let randomNum = Math.floor(Math.random() * 4);
     let randomChosenColor = buttonColors[randomNum]; 
     gamePattern.push(randomChosenColor);
@@ -21,7 +22,28 @@ function nextSequence() {
 $(".btn").on("click", function() {
     let userChosenColor =  $(this).attr("id");
     userClickedPattern.push(userChosenColor);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+    checkAnswer(userClickedPattern.length - 1);
 });
+
+function checkAnswer(currentLevel) {
+    if(gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        if(gamePattern.length === userClickedPattern.length) {
+            setTimeout(function() {
+                nextSequence();
+            }, 1000);
+        }
+    }
+    else {
+        playSound("wrong");
+        $("body").addClass("game-over");
+        $("h1").text("Game Over, Press Any Key to Restart");
+        setTimeout(function() {
+            $("body").removeClass("game-over");
+        }, 200);
+    }
+}
 
 function playSound(name) {
     let audio = new Audio("./sounds/"+name+".mp3");
