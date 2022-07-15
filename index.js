05f9a0b2bd5c39c594bcf33e6727f140d773f9ce
iamjunaidjutt <engr.muhammadjunaid@hotmail.com>
2022-07-15 10:13:19 +0500

    Add keypress function

--- index.js
+++ index.js
@@ -2,21 +2,26 @@
 let gamePattern = [];
 let buttonColors = ["red", "blue", "green", "yellow"];
 let userClickedPattern = [];
+let level = 0;
+let started = false;
 
 function nextSequence() {
     let randomNum = Math.floor(Math.random() * 4);
-    let randomChosenColor = buttonColors[nextSequence()]; 
+    let randomChosenColor = buttonColors[randomNum]; 
     gamePattern.push(randomChosenColor);
     
     $("#"+randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
 
     let audio = new Audio("./sounds/"+randomChosenColor+".mp3");
     audio.play();   
+    level++;
+    $("h1").text("Level " + level);
 }
+
 $(".btn").on("click", function() {
     let userChosenColor =  $(this).attr("id");
     userClickedPattern.push(userChosenColor);
-})
+});
 
 function playSound(name) {
     let audio = new Audio("./sounds/"+name+".mp3");
@@ -29,3 +34,11 @@ function animatePress(currentColor) {
         $("#"+currentColor).removeClass("pressed");
     },100);
 }
+
+$(document).on("keypress", function(event) {
+    if(!started) {
+        $("h1").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
